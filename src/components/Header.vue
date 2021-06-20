b7a323f025427b7fc48150d773c773e54cd4c8eb
Victor <v.priimachuk@gmail.com>
2021-06-20 19:25:00 +0300

    mste

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="Header" class="container-fluid mt-3">
+    <div class="mainbox">
+      <div class="title withoutline">
+        <i class="fas fa-tasks"></i> Менеджер задач
+        <div class="right">
+          <button
+            class="btn btn-primary btn-sm mr-2"
+            v-if="getTasks && getTasks.length > 0"
+            @click="$bvModal.show('CreateSubtaskModal')"
+          >
+            <i class="fas fa-plus mr-2"></i> Создать подзадачу
+          </button>
+          <button
+            class="btn btn-success"
+            @click="$bvModal.show('CreateTaskModal')"
+          >
+            <i class="fas fa-plus mr-2"></i> Создать задачу
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Header",
+  computed: {
+    ...mapGetters("task", ["getTasks"]),
+  },
+};
+</script>
+<style scoped></style>
